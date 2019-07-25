5c5982d6ac5c8d6ce5cf1407c13075161d349d62
masheng <sheng7m@gmail.com>
2019-07-25 11:53:06 +0800

    初始化代码仓库，添加事件部分代码

---
+++ src/foo.ts
@@ -0,0 +1,30 @@
+import { Emitter, Event } from 'base/common/event';
+
+class Document1 {
+  private _onDidChange = new Emitter<string>();
+  public onDidChange: Event<string> = this._onDidChange.event;
+  public setText(value: string): void {
+    this._onDidChange.fire(value);
+  }
+}
+
+class Counter {
+  private _count: number = 0;
+
+  public get count() {
+    return this._count;
+  }
+
+  public reset(): void {
+    this._count = 0;
+  }
+
+  public onEvent(): void {
+    this._count++;
+  }
+}
+
+let counter = new Counter();
+let doc = new Document1();
+let subscription = doc.onDidChange(counter.onEvent, counter);
+
