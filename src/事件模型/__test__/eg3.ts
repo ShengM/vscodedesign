1f18df8e0f97e0bf319b560ca398461bcd077641
masheng <sheng7m@gmail.com>
2019-08-12 17:59:48 +0800

    添加示例3、4

--- src/事件模型/__test__/eg3.ts
+++ src/事件模型/__test__/eg3.ts
@@ -1,5 +1,17 @@
-import {Emitter} from '../base/common/event';
+import { Emitter } from '../base/common/event';
 
-let a=new Emitter({onFirstListenerAdd(){
-  console.log('first listener add');
-}});
+let a = new Emitter({
+  onFirstListenerAdd() {
+    console.log('first listener add');
+  }, onLastListenerRemove() {
+    console.log('last listener remove');
+  }
+});
+
+let subscription = a.event(function () { });
+subscription.dispose();
+
+subscription = a.event(function () { });
+subscription.dispose();
+
+console.log('eg3 done');
